4ad2ac0f9ea065e9d150432639a4b8f081cc3015
ILYA <ilya.kravchuk.02@mail.ru>
2023-04-14 02:07:54 +0600

    Добавлены две функции (удаление одного за другим и вычисление факториала)
    .

--- calculation.py
+++ calculation.py
@@ -24,3 +24,23 @@ def button_clear_all():
     global calc_operator
     calc_operator = ""
     text_input.set("")
+
+# Функция для удаления одного за другим из последнего в записи текстового отображения
+def button_delete():
+    global calc_operator
+    text = calc_operator[:-1]
+    calc_operator = text
+    text_input.set(text)
+
+# Функция для вычисления факториала числа
+def factorial(n):
+    if n==0 or n==1:
+        return 1
+    else:
+        return n*factorial(n-1)
+
+def fact_func():
+    global calc_operator
+    result = str(factorial(int(calc_operator)))
+    calc_operator = result
+    text_input.set(result)
